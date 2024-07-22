5999ca2b837296240ad69b6136f79afd34439909
William Bernting <william.bernting@sembo.se>
2024-07-22 16:34:14 +0200

    remove other pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,59 @@
-import { clientValidatedEnv } from "../config/client-env";
-import styles from "./page.module.css";
+"use client";
+import { BlurFilter } from "pixi.js";
+import { Stage, Container, Sprite, Text, useTick } from "@pixi/react";
+import { use, useEffect, useMemo, useState } from "react";
+import Grid from "./Grid";
+import { Bunny } from "./entities/Bunny";
+import { GRID_SIZE, HEIGHT, WIDTH } from "./constants";
+import { snapSingleCoordToGrid } from "./utils";
+import { Platform } from "./entities/Platform";
+import { EntityRegistryProvider } from "./providers/EntityRegistry";
+import { Background } from "./entities/Background";
 
-export default async function Home() {
-  const { searchResult } = await getProps();
+export const Pixi = () => {
+  const [show, setShow] = useState(false);
+  const blurFilter = useMemo(() => new BlurFilter(4), []);
+
+  useEffect(() => {
+    setShow(true);
+  }, [show, setShow]);
 
   return (
-    <main>
-      <div>
-        <h2>Client-side env</h2>
-        <div>
-          {Object.keys(clientValidatedEnv).map((key) => (
-            <div key={key}>
-              {key}:{" "}
-              {clientValidatedEnv[key as keyof typeof clientValidatedEnv]}
-            </div>
-          ))}
-        </div>
-      </div>
-      <div>
-        <h2>Server data</h2>
-        <div>{searchResult}</div>
-      </div>
-    </main>
+    show && (
+      <Stage
+        //  the random key helps redraw the grid on hot reload
+        key={Math.random()}
+        options={{
+          // backgroundColor: 0xffffff,
+          backgroundColor: 0xccccff,
+        }}
+        width={WIDTH}
+        height={HEIGHT}
+      >
+        <Grid
+          hideGrid
+          color={[0.95, 0.95, 0.95]}
+          lineThickness={1}
+          width={WIDTH}
+          height={HEIGHT}
+          pitch={{ x: GRID_SIZE, y: GRID_SIZE }}
+        />
+        <EntityRegistryProvider>
+          <Container>
+            <Background />
+            <Bunny
+              startX={snapSingleCoordToGrid(GRID_SIZE, WIDTH / 2)}
+              startY={snapSingleCoordToGrid(GRID_SIZE, HEIGHT - GRID_SIZE * 2)}
+            />
+            <Platform
+              startX={GRID_SIZE * 10}
+              startY={snapSingleCoordToGrid(GRID_SIZE, HEIGHT - GRID_SIZE * 2)}
+            />
+          </Container>
+        </EntityRegistryProvider>
+      </Stage>
+    )
   );
-}
-
-export const getProps = async () => {
-  const { MY_SECRET_KEY } = (await import("../config/server-env"))
-    .serverValidatedEnv;
-
-  const result = await (
-    await fetch(`https://www.google.com/search?q=${MY_SECRET_KEY}`)
-  ).text();
-
-  return {
-    searchResult: result.substring(0, 50) + "...",
-  };
 };
+
+export default Pixi;
